15f9c743dcaf475785f3ddfa76914c4d77a82032
katya-sy <katya_sy_28@mail.ru>
2025-01-27 22:40:14 +0900

    finish integer test

--- tests/integer-test.test.ts
+++ tests/integer-test.test.ts
@@ -1,8 +1,6 @@
 import { DatabaseService, User } from "../src/database-service";
 import { UserService } from "../src/user-service";
 
-jest.mock("../src/database-service");
-
 describe("Database service with user service", () => {
   let dbService: DatabaseService;
   let userService: UserService;
@@ -17,11 +15,17 @@ describe("Database service with user service", () => {
     expect(user).toBeUndefined();
   });
 
-  // test("Should return user", () => {
-  //   const user: User = { id: 1, name: "John Doe" };
-  //   dbService.addUser(user);
+  test("Should add and return users", () => {
+    const user1: User = { id: 1, name: "John Doe" };
+    const user2: User = { id: 2, name: "John Blue" };
 
-  //   const addedUser = userService.getUserById(1);
-  //   expect(addedUser).toEqual(user);
-  // });
+    dbService.addUser(user1);
+    dbService.addUser(user2);
+
+    const addedUser1 = userService.getUserById(1);
+    const addedUser2 = userService.getUserById(2);
+
+    expect(addedUser1).toEqual(user1);
+    expect(addedUser2).toEqual(user2);
+  });
 });
